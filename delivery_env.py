aed5b842d06e5a7f91270ee3799f6e170e8a1aa9
Anders <anders.erik.lie@gmail.com>
2021-11-08 11:19:22 -0600

    render

--- delivery_env.py
+++ delivery_env.py
@@ -30,6 +30,8 @@ DTYPE = np.uint8
 
 class DeliveryState:
     def __init__(self, x_lim, y_lim):
+        self.x_lim = x_lim
+        self.y_lim = y_lim
         self.reset()
 
     def reset(self):
@@ -37,11 +39,13 @@ class DeliveryState:
         # Player coordinates
         self.player = np.array([0, 0], dtype=DTYPE)
         # Package Pickup Locations
-        self.pickups = np.zeros(shape=(x_lim, y_lim), dtype=DTYPE)
+        self.pickups = np.zeros(shape=(self.x_lim, self.y_lim), dtype=DTYPE)
         # Current Package Locations, value indicates num packages.
-        self.packages = np.zeros(shape=(x_lim, y_lim), dtype=DTYPE)
+        self.packages = np.zeros(shape=(self.x_lim, self.y_lim), dtype=DTYPE)
         # Package Dropoff Locations
-        self.dropoffs = np.zeros(shape=(x_lim, y_lim), dtype=DTYPE)
+        self.dropoffs = np.zeros(shape=(self.x_lim, self.y_lim), dtype=DTYPE)
+
+        # TODO: Create some pickup / dropoff locations by setting some of those array values to 1.
 
 
     def to_array(self):
@@ -51,10 +55,29 @@ class DeliveryState:
         return (self.player, self.pickups, self.packages, self.dropoffs)
 
     def render(self):
-        print(f'{self.player=}')
-        print(f'{self.pickups=}')
-        print(f'{self.packages=}')
-        print(f'{self.dropoffs=}')
+        # print(f'{self.player=}')
+        # print(f'{self.pickups=}')
+        # print(f'{self.packages=}')
+        # print(f'{self.dropoffs=}')
+
+        for x in range(self.x_lim):
+            for y in range(self.y_lim):
+                ch = ''
+                if self.player[0] == x and self.player[1] == y:
+                    ch += Colors.BG_RED
+                elif self.pickups[x, y] > 0:
+                    ch += Colors.BG_MAGENTA
+                elif self.dropoffs[x, y] > 0:
+                    ch += Colors.BG_GREEN
+
+                ch += str(self.packages[x, y])
+                
+                ch += Colors.RESET
+
+                print(ch, end='')
+                if y < self.y_lim - 1:
+                    print(end=' ')
+            print()
 
 class DeliveryEnv(gym.Env):
     '''
@@ -90,7 +113,7 @@ class DeliveryEnv(gym.Env):
             # Package Locations
             Box(
                 low=np.zeros((x_lim, y_lim)),
-                high=np.full((x_lim, y_lim), 10), # Say max of 10 packages at any given location
+                high=np.full((x_lim, y_lim), 9), # Say max of 9 packages at any given location (so we can display with one char)
                 dtype=DTYPE
             ),
             # Dropoff Locations
@@ -102,16 +125,6 @@ class DeliveryEnv(gym.Env):
         ))
 
         self.state = DeliveryState(x_lim, y_lim)
-        # self.observation_space = gym.spaces.Tuple((gym.spaces.Box(
-        #     low=np.array([0,0], dtype=dtype),
-        #     high=np.array([100, 20000], dtype=dtype),
-        #     dtype=dtype
-        # ), gym.spaces.Discrete(4)))
-        #self.observation_space = gym.spaces.Box(low=np.array([0]), high=np.array([100]))
-
-        #self.observation_space = gym.spaces.Box(low=np.array([0]), high=np.array([100]))
-
-        #_ = self.reset()
         
 
     def reset(self):
@@ -122,7 +135,7 @@ class DeliveryEnv(gym.Env):
         pass
 
     def render(self, mode='human'):
-        pass
+        self.state.render()
     
 if __name__ == '__main__':
     env = DeliveryEnv(3, 3)
@@ -134,3 +147,5 @@ if __name__ == '__main__':
     print('Observation Space Samples:')
     for _ in range(10):
         print(env.observation_space.sample())
+
+    env.render()

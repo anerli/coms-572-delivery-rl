9f93721ee7c8ff328a84be30468b88697912be0b
Anders <anders.erik.lie@gmail.com>
2021-11-08 12:06:37 -0600

    colors

--- delivery_env.py
+++ delivery_env.py
@@ -3,11 +3,26 @@ from gym.spaces import Discrete, Box, Tuple
 import numpy as np
 import random
 
+# https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
 class Colors:
     RED = '\u001b[31m'
+    BLACK = '\u001b[30m'
     BG_RED = '\u001b[41m' # Use for player
+    BG_BRIGHT_RED = '\u001b[41;1m'
     BG_MAGENTA = '\u001b[45m' # Use for pickup
     BG_GREEN = '\u001b[42m' # Use for dropoff
+    BG_BLUE = '\u001b[44m'
+    BG_YELLOW = '\u001b[43m'
+
+    # Blue-ish BG
+    PLAYER = '\u001b[48;2;40;180;255;38;2;0;0;0m'
+
+    # Yellowish BG
+    PICKUP = '\u001b[48;2;200;200;0;38;2;0;0;0m'
+
+    # Dark green BG
+    DROPOFF = '\u001b[48;2;0;200;0;38;2;0;0;0m'
+
     RESET = '\u001b[0m'
 
 # Discrete data type used for np.array representations.
@@ -22,7 +37,7 @@ class DeliveryState:
     def reset(self):
         global DTYPE
         # Player coordinates
-        self.player = np.array([0, 0], dtype=DTYPE)
+        self.player = np.array([1, 0], dtype=DTYPE)
         # Package Pickup Locations
         self.pickups = np.zeros(shape=(self.x_lim, self.y_lim), dtype=DTYPE)
         # Current Package Locations, value indicates num packages.
@@ -31,6 +46,19 @@ class DeliveryState:
         self.dropoffs = np.zeros(shape=(self.x_lim, self.y_lim), dtype=DTYPE)
 
         # TODO: Create some pickup / dropoff locations by setting some of those array values to 1.
+        # Sample without replacement
+        # For random:
+
+        # space = [(x, y) for x in range(self.x_lim) for y in range(self.y_lim)]
+        # pickup_loc = random.choice(space)
+        # space.remove(pickup_loc)
+        # self.pickups
+        # print(space)
+
+        self.pickups[2,2] = 1
+        self.dropoffs[0, 2] = 1
+        self.dropoffs[0, 1] = 1
+
 
 
     def to_array(self):
@@ -40,27 +68,32 @@ class DeliveryState:
         return (self.player, self.pickups, self.packages, self.dropoffs)
 
     def render(self):
-        # print(f'{self.player=}')
-        # print(f'{self.pickups=}')
-        # print(f'{self.packages=}')
-        # print(f'{self.dropoffs=}')
+        print(f'{self.player=}')
+        print(f'{self.pickups=}')
+        print(f'{self.packages=}')
+        print(f'{self.dropoffs=}')
 
-        for x in range(self.x_lim):
-            for y in range(self.y_lim):
+
+        for y in range(self.y_lim):
+            for x in range(self.x_lim):
                 ch = ''
                 if self.player[0] == x and self.player[1] == y:
-                    ch += Colors.BG_RED
+                    # Always end in m, semi colon separated code vals
+                    # 0m: clear
+                    # 38: set color (next args define color)
+                    # 48: set bg color
+                    ch += Colors.PLAYER#Colors.BG_BRIGHT_RED + '\u001b[38;5;232m' #Colors.BLACK
                 elif self.pickups[x, y] > 0:
-                    ch += Colors.BG_MAGENTA
+                    ch += Colors.PICKUP#Colors.BG_MAGENTA
                 elif self.dropoffs[x, y] > 0:
-                    ch += Colors.BG_GREEN
+                    ch += Colors.DROPOFF#Colors.BG_BLUE
 
                 ch += str(self.packages[x, y])
                 
                 ch += Colors.RESET
 
                 print(ch, end='')
-                if y < self.y_lim - 1:
+                if x < self.x_lim - 1:
                     print(end=' ')
             print()
 
@@ -123,14 +156,14 @@ class DeliveryEnv(gym.Env):
         self.state.render()
     
 if __name__ == '__main__':
-    env = DeliveryEnv(3, 3)
-    print('Action Space Shape:', env.action_space.n)
-    print('Action Space Samples:')
-    for _ in range(10):
-        print(env.action_space.sample())
-    print('Observation Space Shape:', env.observation_space.shape)
-    print('Observation Space Samples:')
-    for _ in range(10):
-        print(env.observation_space.sample())
+    env = DeliveryEnv(3, 4)
+    # print('Action Space Shape:', env.action_space.n)
+    # print('Action Space Samples:')
+    # for _ in range(10):
+    #     print(env.action_space.sample())
+    # print('Observation Space Shape:', env.observation_space.shape)
+    # print('Observation Space Samples:')
+    # for _ in range(10):
+    #     print(env.observation_space.sample())
 
     env.render()
